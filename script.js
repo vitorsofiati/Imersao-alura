91fae76c51be455e231a54d9892bf7bc2ae72b32
Vitor Sofiati <vitor.sofiati@live.com>
2023-11-22 10:07:13 -0300

    adicionados novos jogadores através de array e funções ajustadas

--- script.js
+++ script.js
@@ -1,41 +1,46 @@
-
-var paulo = { nome: "Paulo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
-var valentina = { nome: "Valentina", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
-var enzo = { nome: "Enzo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+const listaJogadores = [
+  { nome: "Paulo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },
+  { nome: "Valentina", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },
+  { nome: "Enzo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },
+]
 
 const tabelaJogadores = document.getElementById("tabelaJogadores");
 
 function exibirNaTela() {
-  tabelaJogadores.innerHTML = ` 
+  // limpar a tabela para não duplicar (antes do loop pra n limpar cada linha)
+  tabelaJogadores.innerHTML = "";
+  for (let i = 0; i < listaJogadores.length; i++) {
+    tabelaJogadores.innerHTML += ` 
   <tr>
-  <td>${paulo.nome}</td>
-  <td>${paulo.vitorias}</td>
-  <td>${paulo.empates}</td>
-  <td>${paulo.derrotas}</td>
-  <td>${paulo.pontos}</td>
-  <td><button onClick="adicionarVitoria(paulo)">Vitória</button></td>
-  <td><button onClick="adicionarEmpate(paulo)">Empate</button></td>
-  <td><button onClick="adicionarDerrota(paulo)">Derrota</button></td>
+  <td>${listaJogadores[i].nome}</td>
+  <td>${listaJogadores[i].vitorias}</td>
+  <td>${listaJogadores[i].empates}</td>
+  <td>${listaJogadores[i].derrotas}</td>
+  <td>${listaJogadores[i].pontos}</td>
+  <td><button onClick="adicionarVitoria(${i})">Vitória</button></td>
+  <td><button onClick="adicionarEmpate(${i})">Empate</button></td>
+  <td><button onClick="adicionarDerrota(${i})">Derrota</button></td>
   </tr>
-  `;
+    `;
+    // ${i} passa a referência apenas do índice (que será usado na declaração das funções abaixo). Usar ${array[i]} iria passar o objeto inteiro.
+  }
 }
 
 exibirNaTela();
 
 
-function adicionarVitoria(jogador) {
-  jogador.vitorias++;
-  jogador.pontos = jogador.pontos + 3;
+function adicionarVitoria(index) {
+  listaJogadores[index].vitorias++;
+  listaJogadores[index].pontos += 3;
   exibirNaTela();
 };
 
-function adicionarEmpate(jogador) {
-  jogador.empates++;
-  jogador.pontos++;
+function adicionarEmpate(index) {
+  listaJogadores[index].empates++;
+  listaJogadores[index].pontos++;
   exibirNaTela();
 };
-function adicionarDerrota(jogador) {
-  jogador.derrotas++;
+function adicionarDerrota(index) {
+  listaJogadores[index].derrotas++;
   exibirNaTela();
-  console.log(jogador);
 };
