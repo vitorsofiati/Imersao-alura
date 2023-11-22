15af50f40bcfee904c37a4fc4bc4c19d590b5415
Vitor <vitor.sofiati@live.com>
2023-11-21 22:32:37 -0300

    funcionamento da tabela e dos botoes

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("teste");
+
+var paulo = { nome: "Paulo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+var valentina = { nome: "Valentina", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+var enzo = { nome: "Enzo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 };
+
+const tabelaJogadores = document.getElementById("tabelaJogadores");
+
+function exibirNaTela() {
+  tabelaJogadores.innerHTML = ` 
+  <tr>
+  <td>${paulo.nome}</td>
+  <td>${paulo.vitorias}</td>
+  <td>${paulo.empates}</td>
+  <td>${paulo.derrotas}</td>
+  <td>${paulo.pontos}</td>
+  <td><button onClick="adicionarVitoria(paulo)">Vitória</button></td>
+  <td><button onClick="adicionarEmpate(paulo)">Empate</button></td>
+  <td><button onClick="adicionarDerrota(paulo)">Derrota</button></td>
+  </tr>
+  `;
+}
+
+exibirNaTela();
+
+
+function adicionarVitoria(jogador) {
+  jogador.vitorias++;
+  jogador.pontos = jogador.pontos + 3;
+  exibirNaTela();
+};
+
+function adicionarEmpate(jogador) {
+  jogador.empates++;
+  jogador.pontos++;
+  exibirNaTela();
+};
+function adicionarDerrota(jogador) {
+  jogador.derrotas++;
+  exibirNaTela();
+  console.log(jogador);
+};
