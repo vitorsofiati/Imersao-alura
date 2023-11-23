2ce5f734c60cb57c0c2670cf54821ac68b836073
Vitor Sofiati <vitor.sofiati@live.com>
2023-11-23 17:27:51 -0300

    Função de exclusão de jogador finalizada

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let listaJogadores = [
   { nome: "Valentina", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },
-  { nome: "Enzo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },]
+  { nome: "Enzo", vitorias: 0, empates: 0, derrotas: 0, pontos: 0 },
+]
+
 const botaoNovoJogador = document.getElementById("botao-novo-jogador");
 const submitNovoJogador = document.querySelector(".novo-jogador button");
 const botaoZerar = document.getElementById("botao-zerar");
@@ -22,12 +24,14 @@ function atualizarTabela() {
   <td><button class="botao-tabela" onClick="adicionarVitoria(${i})">Vitória</button></td>
   <td><button class="botao-tabela" onClick="adicionarEmpate(${i})">Empate</button></td>
   <td><button class="botao-tabela" onClick="adicionarDerrota(${i})">Derrota</button></td>
-  <td><button class="icon botao-excluir"></button></td>
+  <td><button class="icon botao-excluir" onClick="excluirJogador(${i})"></button></td>
   </tr>
     `;
     // ${i} acima passa a referência apenas do índice (que será usado na declaração das funções abaixo). Usar ${array[i]} iria passar o objeto inteiro.
   }
 }
+atualizarTabela();
+
 
 function adicionarVitoria(index) {
   listaJogadores[index].vitorias++;
@@ -44,6 +48,14 @@ function adicionarDerrota(index) {
   listaJogadores[index].derrotas++;
   atualizarTabela();
 };
+
+function excluirJogador(index) {
+  // splice remove, substitui ou adiciona itens de array a partir de um indice.
+  // (a partir do indice index, remova 1 item)
+  listaJogadores.splice(index, 1);
+  atualizarTabela();
+}
+
 
 // toggle para o campo de input
 function esconder() {
@@ -99,4 +111,4 @@ atualizarTabela();
 botaoNovoJogador.addEventListener("click", esconder);
 submitNovoJogador.addEventListener("click", inserirJogador);
 botaoZerar.addEventListener("click", zerarPontos);
-botaoRedefinir.addEventListener("click", redefinirTabela)
+botaoRedefinir.addEventListener("click", redefinirTabela);
